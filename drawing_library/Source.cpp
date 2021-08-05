f5bc80be122954ff783fb4ee96b9c3bea26d05db
School_PC <aiden_sanchez>
2021-08-05 10:27:07 -0700

    Added Vector2 operators

--- drawing_library/Source.cpp
+++ drawing_library/Source.cpp
@@ -1,6 +1,12 @@
+#include "helpers/math/math.hpp"
+
 #include <Windows.h>
+#include <iostream>
 
 int main()
 {
-
+	auto result = Vector2(5, 5);
+	auto yes = result + Vector2(5, 5);
+	std::cout << result.x << ", " << result.y << std::endl;
+	std::cout << yes.x << ", " << yes.y << std::endl;
 }
--- drawing_library/helpers/math/math.hpp
+++ drawing_library/helpers/math/math.hpp
@@ -3,10 +3,63 @@
 
 struct Vector2
 {
+	// members
 	int x;
 	int y;
-	const Vector2 operator =(std::pair<int, int>(int x, int y))
-	{
 
+	void swap(Vector2& vec)
+	{
+		*this = vec;
 	}
+
+	Vector2& operator =(Vector2& vec)
+	{
+		if (&vec.x != 0 && &vec.y != 0)
+		{
+			this->swap(vec);
+			return *this;
+		}
+	}
+
+	bool operator ==(Vector2& vec)
+	{
+		return *this == vec;
+	}
+
+	Vector2& operator &(Vector2& vec)
+	{
+		return *this;
+	}
+
+	Vector2& operator ->()
+	{
+		return *this;
+	}
+
+	const Vector2& operator +(const Vector2& vec)
+	{
+		return Vector2(x + vec.x, y + vec.y);
+	}
+
+	void operator +=(const Vector2& vec)
+	{
+		x += vec.x;
+		y += vec.y;
+	}
+
+	Vector2& operator -(const Vector2 vec)
+	{
+		if (&vec.x != 0 && &vec.y != 0)
+			return *this - vec;
+		return *this;
+	}
+
+	Vector2& operator /(const Vector2 vec)
+	{
+		if (&vec.x != 0 && &vec.y != 0)
+			return *this / vec;
+		return *this;
+	}
+
+	Vector2(int x, int y) : x(x), y(y) {};
 };
