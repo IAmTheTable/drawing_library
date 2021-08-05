dde1f52d0abf234e356acc3e36177c1109544d9f
Administrator <Administrator@MS-CODECART2-17>
2021-08-05 09:27:49 -0700

    Add project files.

---
+++ drawing_library/drawing/drawing.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <Windows.h>
+#include "../helpers/math/math.hpp"
+
+namespace drawing
+{
+	class drawing
+	{
+	public:
+		template<class T>
+		bool draw_circle()
+	};
+}
---
+++ drawing_library/helpers/math/math.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <utility>
+
+struct Vector2
+{
+	int x;
+	int y;
+	const Vector2 operator =(std::pair<int, int>(int x, int y))
+	{
+
+	}
+};
