dbdfb9be1988fce7c1171b2e79b1099c2c7dd168
School_PC <yaboiaidencoder@gmail.com>
2021-08-06 10:27:59 -0700

    use of basic function

--- drawing_library/Source.cpp
+++ drawing_library/Source.cpp
@@ -1,12 +1,10 @@
 #include "helpers/math/math.hpp"
+#include "drawing/drawing.hpp"
 
 #include <Windows.h>
 #include <iostream>
 
 int main()
 {
-	auto result = Vector2(5, 5);
-	auto yes = result + Vector2(5, 5);
-	std::cout << result.x << ", " << result.y << std::endl;
-	std::cout << yes.x << ", " << yes.y << std::endl;
+	drawing::drawing().draw_circle(Vector2{ 2,2 }, 3);
 }
