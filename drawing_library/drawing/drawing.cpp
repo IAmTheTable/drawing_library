24352bf82d9d0da04acd9383e29595aaf847a46c
School_PC <yaboiaidencoder@gmail.com>
2021-08-06 10:19:29 -0700

    Fixed typo in drawing.cpp where it had 3 params

--- drawing_library/drawing/drawing.cpp
+++ drawing_library/drawing/drawing.cpp
@@ -4,7 +4,7 @@ bool drawing::drawing::draw_circle(Vector2 position, int radius)
 {
 	RECT size;
 	GetUpdateRect(GetDesktopWindow(), &size, false);
-	auto screen_size = Vector2(size.left - size.right, size.top, size.bottom);
+	auto screen_size = Vector2(size.left - size.right, size.top - size.bottom);
 
 	return false;
 }
